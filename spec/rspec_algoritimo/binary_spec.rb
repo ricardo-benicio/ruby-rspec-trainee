f68f46f482ed9dc43032ff1c1ea8bfebf6a3b8c1
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-04-28 16:59:13 -0300

    Test: add arquivo spec para confirmação dos resultados.

---
+++ spec/rspec_algoritimo/binary_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'algoritimos/binary_search'
+
+RSpec.describe BinarySearch do
+  context 'Binary search' do
+    it 'Elemento encontrado' do
+      arr = [5, 7, 9, 15, 34, 65]
+      element = 34
+      last = arr.length - 1
+
+      result = BinarySearch.new.search(arr, element, 0, last)
+
+      expect(result).to eq(4)
+    end
+
+    it 'Elemento não encontrado' do
+      arr = [5, 7, 9, 15, 34, 65]
+      element = 13
+      last = arr.length - 1
+
+      result = BinarySearch.new.search(arr, element, 0, last)
+
+      expect(result).to eq(3)
+    end
+  end
+end
