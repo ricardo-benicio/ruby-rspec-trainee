be458fb4f658f8f64df766a22cd62f16a79b7c92
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-01 12:45:46 -0300

    Test: testando os testes binary_spec e add configurações no spec_helper.

--- spec/rspec_algoritimo/binary_spec.rb
+++ spec/rspec_algoritimo/binary_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe BinarySearch do
 
     it 'Elemento não encontrado' do
       arr = [5, 7, 9, 15, 34, 65]
-      element = 13
+      element = 15
       last = arr.length - 1
 
       result = BinarySearch.new.search(arr, element, 0, last)
