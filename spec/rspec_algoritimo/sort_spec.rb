d3ef380ae5a1d92315592813173f4f4f90ea8744
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-03 15:56:19 -0300

    Refactor: alterando variaveis para usar em classes.

---
+++ spec/rspec_algoritimo/sort_spec.rb
@@ -0,0 +1,16 @@
+require 'algoritimos/sort'
+require 'spec_helper'
+
+RSpec.describe Sort, :focus do
+    it 'ordenação completa' do
+        unsorted_array = [30,20,10]
+
+       
+        result = Sort.new.quick(unsorted_array)
+        
+        sorted_array = [10,20,30]
+        expect(result).to eq(sorted_array)
+    end
+
+
+end
