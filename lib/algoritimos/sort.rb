cecacfc7607f6668e2cf067c023ccfbdc5d5d8a2
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-04 13:55:25 -0300

    Refactor: organizando as variavel e renomeando para maior compreensão.

--- lib/algoritimos/sort.rb
+++ lib/algoritimos/sort.rb
@@ -1,32 +1,30 @@
 class Sort
-    attr_acessor :unsorted_array
-
+    attr_accessor :unsorted_array
+    
     def initialize
-        @unsorted_array = unsorted_array
-        
+        @unsorted_array = [30,20,10]
+        @pivot_index = 0
     end
     
     
-    def quick()
+    def order
         return @unsorted_array if @unsorted_array == @unsorted_array.sort
-                    
-            # tornar o ultimo elemento o elemento pivo
-            pivot_index = @unsorted_array.length - 1
+                   
+        @pivot_index = @unsorted_array.length - 1
 
-            i = -1 #i começa com -1
-
-            #transversal de 10 a 50
+            i = -1 
+            
             (0..@unsorted_array.length-2).each do |j|
-                if @unsorted_array[j] <= @unsorted_array[pivot_index]
-                    #se j for menor que pivo, troque i por j
+                if @unsorted_array[j] <= @unsorted_array[@pivot_index]
+                
                     i += 1
                     @unsorted_array[j], @unsorted_array[i] = @unsorted_array[i], @unsorted_array[j]
                 end                
                    
-        #troque i+1 por pivo
-        @unsorted_array[pivot_index], @unsorted_array[i+1] = @unsorted_array[i+1], @unsorted_array[pivot_index]
-        p "unsorted_array is"
-        p @unsorted_array        
+        @unsorted_array[@pivot_index], @unsorted_array[i+1] = @unsorted_array[i+1], @unsorted_array[@pivot_index]       
         end   
     end
 end
+
+result = Sort.new.order
+p result
--- spec/rspec_algoritimo/sort_spec.rb
+++ spec/rspec_algoritimo/sort_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 
 RSpec.describe Sort, :focus do
     it 'ordenação completa' do
-        unsorted_array = [30,20,10]
+        @unsorted_array = [30,20,10]
 
        
-        result = Sort.new.quick(unsorted_array)
+        result = Sort.new.order
         
         sorted_array = [10,20,30]
         expect(result).to eq(sorted_array)
