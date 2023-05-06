344283904006232d8487cbafecfb78c99b6ac29a
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-06 14:40:04 -0300

    refactor: alterando variaves e trocando os codigos da classe sort

--- lib/algoritimos/sort.rb
+++ lib/algoritimos/sort.rb
@@ -1,32 +1,21 @@
 class Sort
 
-    attr_accessor :pivot_index, :unsorted_array
-
-    def initialize
-        @unsorted_array = [70,20,30,50,10]
-        @pivot_index = []
-    end
-    
-    
-    def order
-        return @unsorted_array if @unsorted_array == @unsorted_array.sort
-                   
-        @pivot_index = @unsorted_array.length - 1
-
-            i = -1
-            
-            (0..@unsorted_array.length-2).each do |j|
-                if @unsorted_array[j] <= @unsorted_array[@pivot_index]
-                
-                    i += 1
-                    @unsorted_array[j], @unsorted_array[i] = @unsorted_array[i], @unsorted_array[j]
-                end                
-                   
-        @unsorted_array[@pivot_index], @unsorted_array[i+1] = @unsorted_array[i+1], @unsorted_array[@pivot_index]       
-        end   
-        @unsorted_array
+    def initialize(array)
+        @array = array
     end
 
-
-
+    def self.buble_sort(array)
+        n = array.size
+        loop do
+            swapped = false
+            (n-1).times do |i|
+                if array[i] > array[i+1]
+                    array[i], array[i+1] = array[i+1], array[i]
+                    swapped = true                    
+                end
+            end
+            break unless swapped
+        end
+        array
+    end
 end
--- spec/rspec_algoritimo/sort_spec.rb
+++ spec/rspec_algoritimo/sort_spec.rb
@@ -2,14 +2,13 @@ require 'algoritimos/sort'
 require 'spec_helper'
 
 RSpec.describe Sort, :focus do
-    it 'ordenação completa' do
-        @unsorted_array
-       
-        result = Sort.new.order
-        
-        sorted_array = [10,20,30,50,70]
-        expect(result).to eq(sorted_array)
-    end
+        unsorted_array = [15,95,45,128,25,65]
+        sorted_array   = [15,25,45,65,95,128]
 
-
+        describe "#buble_sort" do
+            it "true sorted array" do
+                expect(Sort.buble_sort(unsorted_array)).to eq(sorted_array)
+            end
+        end
+    
 end
