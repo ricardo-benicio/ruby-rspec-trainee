116ce38d2829e23246eb4e41c899a32bb2a37a3c
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-15 16:10:09 -0300

    Test: adicionando testes para novos algoritimos incrimentados.

--- spec/rspec_algoritimo/sort_spec.rb
+++ spec/rspec_algoritimo/sort_spec.rb
@@ -5,10 +5,28 @@ RSpec.describe Sort, :focus do
     unsorted_array = [15,95,45,128,25,65]
     sorted_array   = [15,25,45,65,95,128]
 
-    describe "#buble_sort" do
+    describe "buble_sort" do
         it "true sorted array" do
             expect(Sort.buble(unsorted_array)).to eq(sorted_array)
         end
     end
+
+    describe "selection_sort" do
+        it "true sorted array" do
+            expect(Sort.selection(unsorted_array)).to eq(sorted_array)
+        end
+    end
     
+    describe "insertion_sort" do
+        it "true sorted array" do
+            expect(Sort.insertion(unsorted_array)).to eq(sorted_array)
+        end
+    end
+
+    describe "quick_sort" do
+        it "true sorted array" do
+            expect(Sort.quick(unsorted_array)).to eq(sorted_array)
+        end
+    end
+
 end
