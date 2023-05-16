55556ac7431dd151765e5d5eb37ff168b7c756aa
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-16 16:21:07 -0300

    Refactor: alterando e organizando variaveis de tratamento do algoritimos.

--- spec/rspec_algorithms/sort_spec.rb
+++ spec/rspec_algorithms/sort_spec.rb
@@ -1,26 +1,27 @@
-require 'algoritimos/sort'
-require 'spec_helper'
+# frozen_string_literal: true
+
+require "algorithms/sort"
+require "spec_helper"
 
 RSpec.describe Sort, :focus do
-    unsorted_array = [15,95,45,128,25,65]
-    sorted_array   = [15,25,45,65,95,128]
+  unsorted_array = [15, 95, 45, 128, 25, 65]
+  sorted_array   = [15, 25, 45, 65, 95, 128]
 
-    describe "bubble_sort" do
-        it "true sorted array" do
-            expect(Sort.new(unsorted_array).bubble).to eq(sorted_array)
-        end
+  describe "bubble_sort" do
+    it "true sorted array" do
+      expect(Sort.new(unsorted_array).bubble).to eq(sorted_array)
     end
+  end
 
-    describe "selection_sort" do
-        it "true sorted array" do
-            expect(Sort.new(unsorted_array).selection).to eq(sorted_array)
-        end
+  describe "selection_sort" do
+    it "true sorted array" do
+      expect(Sort.new(unsorted_array).selection).to eq(sorted_array)
     end
-    
-    describe "insertion_sort" do
-        it "true sorted array" do
-            expect(Sort.new(unsorted_array).insertion).to eq(sorted_array)
-        end
-    end
+  end
 
+  describe "insertion_sort" do
+    it "true sorted array" do
+      expect(Sort.new(unsorted_array).insertion).to eq(sorted_array)
+    end
+  end
 end
