9bc0066fe40a2ff2229c5287d5826ca0724edf1b
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-05-22 17:31:14 -0300

    Feat: add algoritimo search and test. Reafactore files sort.

--- lib/sort_algorithms/run.rb
+++ lib/sort_algorithms/run.rb
@@ -1,6 +1,6 @@
 require 'sort_algorithms/application'
 
-class Run < App::Sort
+class Run < Sort
     def initialize(array)
         @array = array
       end
--- spec/algorithms/sort_spec.rb
+++ spec/algorithms/sort_spec.rb
@@ -2,14 +2,14 @@ require "sort_algorithms/application"
 require 'sort_algorithms/run'
 require "spec_helper"
 
-RSpec.describe Sort do
+RSpec.describe Run do
   unsorted_array = [15, 95, 25, 128, 45, 65]
   sorted_array   = [15, 25, 45, 65, 95, 128]
 
 context 'Comparator arrays' do 
   describe 'bubble_sort' do
       it 'true sorted array' do
-        expect(Run::Sort.new(unsorted_array).bubble).to eql(sorted_array)
+        expect(Run.new(unsorted_array).bubble).to eql(sorted_array)
       end
     end
 
